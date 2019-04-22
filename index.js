5fb50728cd497c3272a8c3d3c4d11d9c905a2351
Dikla Beauliere <dikla.beauliere@gmail.com>
2019-04-22 18:45:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ function nowServing(joeDeli){
   }
 }
 
-function takeANumber(joeDeli, name){
+/*function takeANumber(joeDeli, name){
   joeDeli.push(name)
   return `Welcome, ${name}. You are number ${joeDeli.length} in line.`
-}
-function takeANumber( name){
-  joeDeli.push(name)
-  return `Welcome, You are number ${joeDeli.length} in line.`
+}*/
+function takeANumber( joeDeli){
+  joeDeli +=1;
+  return `Welcome, You are number ${joeDeli.length } in line.`
 }
 
 
